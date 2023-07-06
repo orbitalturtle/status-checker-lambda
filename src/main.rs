f934b39482dc7798d6d634fbdd5e1631cad6f7a2
Orbital <orbitalturtle@protonmail.com>
2023-07-06 17:13:44 -0500

    Make basic request to provided event url

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
-use aws_lambda_events::event::cloudwatch_events::CloudWatchEvent;use lambda_runtime::{run, service_fn, Error, LambdaEvent};
+use reqwest;
 
+use aws_lambda_events::event::cloudwatch_events::CloudWatchEvent;
+use lambda_runtime::{run, service_fn, Error, LambdaEvent};
 
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-/// - https://github.com/aws-samples/serverless-rust-demo/
+// Simply check if we can connect to url. If not, we need to send a notification.
+#[tracing::instrument(skip(event), fields(req_id = %event.context.request_id))]
 async fn function_handler(event: LambdaEvent<CloudWatchEvent>) -> Result<(), Error> {
+    let detail = event
+        .payload
+        .detail
+        .expect("Payload doesn't have needed url field");
+
+    tracing::info!("Checking health of {:?}", detail["url"]);
+
     // Extract some useful information from the request
+    let resp = reqwest::get(detail["url"].to_string()).await?;
+    if resp.status().is_success() {
+        tracing::info!("Success!");
+    } else if resp.status().is_server_error() {
+        tracing::info!("Server error!");
+    } else {
+        tracing::info!("Something else happened. Status: {:?}", resp.status());
+    }
 
     Ok(())
 }
@@ -19,7 +33,7 @@ async fn main() -> Result<(), Error> {
         // disable printing the name of the module in every log line.
         .with_target(false)
         // disabling time is handy because CloudWatch will add the ingestion time.
-        .without_time()
+        //.without_time()
         .init();
 
     run(service_fn(function_handler)).await
