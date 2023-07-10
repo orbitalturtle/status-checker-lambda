d3a6b0c0b6afffcad56ef87c162b76c8ebdb0b6c
Orbital <orbitalturtle@protonmail.com>
2023-07-09 19:19:57 -0500

    Send Slack message in the event pinging the endpoint fails

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use reqwest;
-
 use aws_lambda_events::event::cloudwatch_events::CloudWatchEvent;
 use lambda_runtime::{run, service_fn, Error, LambdaEvent};
+use reqwest;
+use reqwest::header::CONTENT_TYPE;
+use std::env;
 
-// Simply check if we can connect to url. If not, we need to send a notification.
+// Simply check if we can connect to url. If not, we need to send a Slack notification.
 #[tracing::instrument(skip(event), fields(req_id = %event.context.request_id))]
 async fn function_handler(event: LambdaEvent<CloudWatchEvent>) -> Result<(), Error> {
     let detail = event
@@ -19,11 +20,27 @@ async fn function_handler(event: LambdaEvent<CloudWatchEvent>) -> Result<(), Err
         tracing::info!("Success!");
     } else if resp.status().is_server_error() {
         tracing::info!("Server error!");
+        send_slack_notification(&resp.text().await.unwrap()).await;
     } else {
         tracing::info!("Something else happened. Status: {:?}", resp.status());
     }
 
     Ok(())
+}
+
+async fn send_slack_notification(msg: &str) {
+    let token =
+        env::var("SLACK_BOT_TOKEN")
+        .expect("slack bot token is not set.");
+    
+    let params = [("token", token), ("channel", String::from("notifications")), ("text", String::from(msg))];
+    let client = reqwest::Client::new();
+    let _ = client.post("https://slack.com/api/chat.postMessage")
+        .header(CONTENT_TYPE, "application/x-www-form-urlencoded")
+        .form(&params)
+        .send()
+        .await
+        .expect("Slack API call error");
 }
 
 #[tokio::main]
@@ -33,7 +50,7 @@ async fn main() -> Result<(), Error> {
         // disable printing the name of the module in every log line.
         .with_target(false)
         // disabling time is handy because CloudWatch will add the ingestion time.
-        //.without_time()
+        .without_time()
         .init();
 
     run(service_fn(function_handler)).await
